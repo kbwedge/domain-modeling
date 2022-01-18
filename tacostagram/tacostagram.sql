4f65847df32eb7541abe4494b19348dbf1cf7f4b
kbwedge <kevin.wedge@kellogg.northwestern.edu>
2022-01-18 17:24:46 +0000

    Tacostagram Complete

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,36 @@
 
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  file_name TEXT,
+  user_id INTEGER,
+  time TEXT,
+  caption TEXT
+);
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_name TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT
+);
+
+CREATE TABLE comment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    body TEXT,
+    post_id INTEGER,
+    user_id INTEGER
+);
+
+CREATE TABLE like (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER,
+    user_id INTEGER
+);
+
+CREATE TABLE follow_relationship (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    follower_id INTEGER,
+    followee_id INTEGER
+);
+
